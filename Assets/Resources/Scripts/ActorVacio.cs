33a0d53b13e919267c51739302282a290cc94c6a
nadiaxm5 <montana@uji.es>
2026-02-11 13:38:51 +0100

    Guardar

--- Assets/Resources/Scripts/ActorVacio.cs
+++ Assets/Resources/Scripts/ActorVacio.cs
@@ -6,12 +6,12 @@ public class ActorVacio : MonoBehaviour {
     private Dictionary<string, float> timers = new Dictionary<string, float>();
     void FixedUpdate(){
         {
-            Action.Move("5","0","0","0",gameObject,scopeList);
+            Action.Spawn("Environment", gameObject, "#SunPosition.x", "", "", "", "", "", scopeList);
         }
     }
     public Dictionary<string, GameObject> scopeList = new Dictionary<string, GameObject>();
     void Start() {
-        scopeList = Utils.CreateScope(gameObject.GetInstanceID(),"Move(5,0,0,0)");
+        scopeList = Utils.CreateScope(gameObject.GetInstanceID(),"Spawn(Environment,,#SunPosition.x,,,,,)");
         if (Active) gameObject.SetActive(true);
         else gameObject.SetActive(false);
     }
