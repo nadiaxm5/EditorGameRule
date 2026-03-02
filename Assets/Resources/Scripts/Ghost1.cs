dcfae69bdd0ff00fa378295871e22feb78444b5d
JaumeLloretRubio <jaulloru@gmail.com>
2026-03-02 12:57:23 +0100

    John Lemon funcional
    Todo Ok

---
+++ Assets/Resources/Scripts/Ghost1.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Ghost1 : MonoBehaviour {
+    public bool Active = true;
+    public float speed=1.0f;
+    public float zTarget=6.4f;
+    public float z0=-4.5f;
+    public float z1=6.4f;
+    public Dictionary<string, float> propertyList = new Dictionary<string, float>();
+    private Dictionary<string, float> timers = new Dictionary<string, float>();
+    void FixedUpdate(){
+        {
+            Action.NavigateTo("this.speed","this.x","this.y","this.zTarget",gameObject,scopeList);
+        }
+        if(Condition.Compare("this.z>=this.z1",scopeList)){
+            Action.Edit("this.zTarget","this.z0",scopeList);
+        }
+        if(Condition.Compare("this.z<=this.z0",scopeList)){
+            Action.Edit("this.zTarget","this.z1",scopeList);
+        }
+    }
+    public Dictionary<string, GameObject> scopeList = new Dictionary<string, GameObject>();
+    void Start() {
+        scopeList = Utils.CreateScope(gameObject.GetInstanceID(),"NavigateTo(this.speed,this.x,this.y,this.zTarget);Compare(this.z>=this.z1);Edit(this.zTarget,this.z0);Compare(this.z<=this.z0);Edit(this.zTarget,this.z1)");
+        if (Active) gameObject.SetActive(true);
+        else gameObject.SetActive(false);
+    }
+    void Awake() {
+        propertyList = Utils.CreateProperties("speed=1.0;zTarget=6.4;z0=-4.5;z1=6.4");
+    }
+}
---
+++ Assets/Resources/Scripts/Ghost4.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Ghost4 : MonoBehaviour {
+    public bool Active = true;
+    public float delta=0.5f;
+    public float speed=1.0f;
+    public float xTarget=7.4f;
+    public float zTarget=-2.0f;
+    public float x0=3.2f;
+    public float x1=7.4f;
+    public float z0=-5f;
+    public float z1=-2f;
+    public Dictionary<string, float> propertyList = new Dictionary<string, float>();
+    private Dictionary<string, float> timers = new Dictionary<string, float>();
+    void FixedUpdate(){
+        {
+            Action.NavigateTo("this.speed","this.xTarget","this.y","this.zTarget",gameObject,scopeList);
+        }
+        if(Condition.Compare("this.z<this.z0+this.delta",scopeList) && Condition.Compare("this.z>this.z0-this.delta",scopeList) && Condition.Compare("this.x<this.x0+this.delta",scopeList) && Condition.Compare("this.x>this.x0-this.delta",scopeList)){
+            Action.Edit("this.xTarget","this.x1",scopeList);
+            Action.Edit("this.zTarget","this.z1",scopeList);
+        }
+        if(Condition.Compare("this.z<this.z1+this.delta",scopeList) && Condition.Compare("this.z>this.z1-this.delta",scopeList) && Condition.Compare("this.x<this.x1+this.delta",scopeList) && Condition.Compare("this.x>this.x1-this.delta",scopeList)){
+            Action.Edit("this.xTarget","this.x0",scopeList);
+            Action.Edit("this.zTarget","this.z0",scopeList);
+        }
+    }
+    public Dictionary<string, GameObject> scopeList = new Dictionary<string, GameObject>();
+    void Start() {
+        scopeList = Utils.CreateScope(gameObject.GetInstanceID(),"NavigateTo(this.speed,this.xTarget,this.y,this.zTarget);Compare(this.z<this.z0+this.delta);Compare(this.z>this.z0-this.delta);Compare(this.x<this.x0+this.delta);Compare(this.x>this.x0-this.delta);Edit(this.xTarget,this.x1);Edit(this.zTarget,this.z1);Compare(this.z<this.z1+this.delta);Compare(this.z>this.z1-this.delta);Compare(this.x<this.x1+this.delta);Compare(this.x>this.x1-this.delta);Edit(this.xTarget,this.x0);Edit(this.zTarget,this.z0)");
+        if (Active) gameObject.SetActive(true);
+        else gameObject.SetActive(false);
+    }
+    void Awake() {
+        propertyList = Utils.CreateProperties("delta=0.5;speed=1.0;xTarget=7.4;zTarget=-2.0;x0=3.2;x1=7.4;z0=-5;z1=-2");
+    }
+}
