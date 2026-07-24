331615a4c26a3cdd5282b7b62e8342dae94954c0
nadiaxm5 <montana@uji.es>
2026-07-24 08:43:15 +0200

    User study

--- Assets/Resources/Scripts/Enemy.cs
+++ Assets/Resources/Scripts/Enemy.cs
@@ -6,12 +6,12 @@ public class Enemy : MonoBehaviour {
     private Dictionary<string, float> timers = new Dictionary<string, float>();
     void FixedUpdate(){
         {
-            Action.NavigateTo("5","Player.x","Player.y","Player.z",gameObject,scopeList);
+            Action.NavigateTo("5","this.x","this.y","this.z",gameObject,scopeList);
         }
     }
     public Dictionary<string, GameObject> scopeList = new Dictionary<string, GameObject>();
     void Start() {
-        scopeList = Utils.CreateScope(gameObject.GetInstanceID(),"NavigateTo(5,Player.x,Player.y,Player.z)");
+        scopeList = Utils.CreateScope(gameObject.GetInstanceID(),"NavigateTo(5,this.x,this.y,this.z)");
         if (Active) gameObject.SetActive(true);
         else gameObject.SetActive(false);
     }
